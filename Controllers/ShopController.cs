5e9be2b330a2ba7ca566cbb7b745bf0e576b1ff1
GadirRustamzada <rusrmzadgdir@gmail.com>
2025-12-15 15:55:35 +0400

    fronttoback

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProniaA.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult wishlist()
+        {
+            return View();
+        }
+        public IActionResult SinglePage()
+        {
+            return View();
+        }
+    }
+}
